ae5d2e6e188e94c61298b971367909e3f14b368e
Samuca-sys <samuel.mferreira@hotmail.com>
2020-08-10 20:18:44 -0300

    chore: add PageHeader description

--- aulas/web/src/pages/TeacherForm/index.tsx
+++ aulas/web/src/pages/TeacherForm/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import PageHeader from '../../Components/PageHeader';
-
+import PageHeader from "../../Components/PageHeader";
 
 function TeacherForm() {
-    return (
-        <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas"/>
-        </div>
-    )
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro é preencher esse formulário de inscrição"
+      />
+    </div>
+  );
 }
 
 export default TeacherForm;
