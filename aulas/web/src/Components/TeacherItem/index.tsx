5a4ec7e9e691bc205447af28e66ba4f65e633eec
Samuca-sys <samuel.mferreira@hotmail.com>
2020-08-13 06:51:41 -0300

    chore: add Teacher interfaces

--- aulas/web/src/Components/TeacherItem/index.tsx
+++ aulas/web/src/Components/TeacherItem/index.tsx
@@ -1,43 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css';
+import "./styles.css";
 
-function TeacherItem() {
-    return (
-        <article className="teacher-item">
-            <header>
-                <img src="https://avatars1.githubusercontent.com/u/41910801?s=460&v=4" alt="Samuel Macedo" />
-                <div>
-                    <strong>Samuel Macedo</strong>
-                    <span>Química</span>
-                </div>
-            </header>
-
-            <p>
-                Entusiasta das melhores tecnologias de química avançada
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório epor mudar a vida das pessoas através de experiências.
-            </p>
-
-            <footer>
-                <p>
-                    Preço/hora 
-                    <strong>80,00</strong>
-                </p>
-                <button type="button">
-                    <img src={whatsappIcon} alt="whatsapp"/>
-                    Entrar em contato
-                </button>
-            </footer>
-        </article>
-    );
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
 }
 
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>{teacher.cost}</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
 export default TeacherItem;
-
-
-
-
-
